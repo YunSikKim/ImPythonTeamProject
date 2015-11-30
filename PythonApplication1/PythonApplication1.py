05243a0519d3ba8bd641972b2431141bf91ae7c3
YunSikKim <seneition@naver.com>
2015-11-30 22:40:44 +0900

    WebServer linking

--- PythonApplication1/PythonApplication1.py
+++ PythonApplication1/PythonApplication1.py
@@ -1,32 +1,52 @@
 ﻿from urllib.request import urlopen
+import urllib.parse
 from bs4 import BeautifulSoup
+import http.server
+from http.server import BaseHTTPRequestHandler, HTTPServer
 import json
 
-url = 'http://openapi.e-gen.or.kr/openapi/service/rest/ErmctInfoInqireService/getEgytListInfoInqire?&Q0=%EC%84%9C%EC%9A%B8%ED%8A%B9%EB%B3%84%EC%8B%9C&Q1=%EC%A2%85%EB%A1%9C%EA%B5%AC&numOfRows=100&ServiceKey=FHlV2aXJriahVZyA8K86zVh2MbfmzRPOsXf9N5eHFWZaZBQ66vkxEI8sXLCFMHJ1SveL1Yu5bNpXty9kGs243g%3D%3D'
-data = urlopen(url)
+class DefaultHttpServerHandler(BaseHTTPRequestHandler):
+    def do_GET(self):
+        param = ''
+        url = ''
+        c = self.path
+        temp = c.split("?")
+        query_components = list(c.split("=") for c in temp[1].split("&"))
+        for query in query_components:
+            param += "&" + query[0] + "=" + query[1]
+        url = 'http://openapi.e-gen.or.kr/openapi/service/rest/ErmctInfoInqireService/getEgytListInfoInqire?' + param + '&numOfRows=100&ServiceKey=FHlV2aXJriahVZyA8K86zVh2MbfmzRPOsXf9N5eHFWZaZBQ66vkxEI8sXLCFMHJ1SveL1Yu5bNpXty9kGs243g%3D%3D'
+        data = urlopen(url)
 
-soup = BeautifulSoup(data, from_encoding="utf-8")
+        soup = BeautifulSoup(data, from_encoding="utf-8")
 
-elist = []
+        elist = []
 
-for items in soup.findAll('item'):
-    temp = []
-    for item in items: 
-        temp.append(item.string)
-    elist.append(temp)
+        for items in soup.findAll('item'):
+            temp = []
+            for item in items: 
+                temp.append(item.string)
+            elist.append(temp)
+        
+        jsondata = []
+        
+        dic = {}
+        
+        for item in elist:
+            dic = dict(name=item[3], emclsName=item[2], address=item[0], representTel=item[4], emergencyTel=item[5], latitude=item[9], longitude=item[10])
+            jsondata.append(dic)
+        
+        for item in jsondata:
+            print(item)
 
-jsondata = []
+        data2 = json.dumps(jsondata, ensure_ascii=False)
 
-dic = {}
+        # with open('data.json', 'w', encoding='utf-8') as f:
+        #     f.write(data2)
+        
+        self.wfile.write(data2.encode("utf-8"));
+        # self.wfile.write('HELLO WORLD')
 
-for item in elist:
-    dic = dict(name=item[3], emclsName=item[2], address=item[0], representTel=item[4], emergencyTel=item[5], latitude=item[9], longitude=item[10])
-    jsondata.append(dic)
-
-for item in jsondata:
-    print(item)
-
-data2 = json.dumps(jsondata, ensure_ascii=False)
-
-with open('data.json', 'w', encoding='utf-8') as f:
-    f.write(data2)
+if __name__ == '__main__':
+	httpd = HTTPServer(('', 8881), DefaultHttpServerHandler)
+	print ('Started Webserver port 8888...')
+	httpd.serve_forever()
